fd8e8b9c9a2fc76abbb0482c4294000b7db1c2d6
Lavdriim-m <lavdrim.mustafi03@gmail.com>
2025-01-07 15:35:00 +0100

    I created "Bid.cs", "Comment.cs" as well as "Listing.cs" to generally immitate some of the most basic parts of an Auction App.

--- Auctions/Data/ApplicationDbContext.cs
+++ Auctions/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Auctions.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Auctions.Data;
@@ -9,4 +10,7 @@ public class ApplicationDbContext : IdentityDbContext
         : base(options)
     {
     }
+    public DbSet<Listing> Listings { get; set; }
+    public DbSet<Bid> Bids { get; set; }
+    public DbSet<Comment> Comments { get; set; }
 }
